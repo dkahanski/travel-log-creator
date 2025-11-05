989bcefd8ed4cd5b3489381c770f4370ad9ca989
Dmitry M. Kaganski <d.kagansky@gmail.com>
2025-11-05 15:47:46 +0300

    add lint settings

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import antfu from "@antfu/eslint-config";
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt(
+  antfu({
+    type: "app",
+    vue: true,
+    nuxt: true,
+    standalone: false,
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      indent: 2,
+      semi: true,
+      quotes: "double",
+    },
+  }),
+);
